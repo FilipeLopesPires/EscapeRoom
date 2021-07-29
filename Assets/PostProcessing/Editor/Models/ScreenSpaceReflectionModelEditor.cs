13eb96c1053f0fb0d12beda87c04ab5bb9d413bb
Filipe Pires <filipesnetopires@ua.pt>
2021-07-29 16:23:20 +0100

    Project added.

---
+++ Assets/PostProcessing/Editor/Models/ScreenSpaceReflectionModelEditor.cs
@@ -0,0 +1,101 @@
+ R (section*.104) 290 0 R (section*.106) 294 0 R (section*.156) 366 0 R (section*.157) 370 0 R (section*.91) 262 0 R]
+/Limits [(section*.101) (section*.91)]
+>> endobj
+918 0 obj <<
+/Names [(section*.94) 266 0 R (section*.96) 270 0 R (section.1) 30 0 R (section.21) 94 0 R (section.25) 110 0 R (section.3) 34 0 R]
+/Limits [(section*.94) (section.3)]
+>> endobj
+919 0 obj <<
+/Names [(section.37) 154 0 R (section.39) 158 0 R (section.41) 166 0 R (section.42) 170 0 R (section.44) 174 0 R (section.56) 182 0 R]
+/Limits [(section.37) (section.56)]
+>> endobj
+920 0 obj <<
+/Names [(section.70) 214 0 R (section.81) 230 0 R (section.83) 238 0 R (section.88) 258 0 R (subsection.107) 298 0 R (subsection.111) 302 0 R]
+/Limits [(section.70) (subsection.111)]
+>> endobj
+921 0 obj <<
+/Names [(subsection.113) 306 0 R (subsection.114) 310 0 R (subsection.115) 314 0 R (subsection.122) 318 0 R (subsection.123) 322 0 R (subsection.130) 326 0 R]
+/Limits [(subsection.113) (subsection.130)]
+>> endobj
+922 0 obj <<
+/Names [(subsection.134) 330 0 R (subsection.137) 334 0 R (subsection.139) 338 0 R (subsection.140) 342 0 R (subsection.141) 346 0 R (subsection.143) 350 0 R]
+/Limits [(subsection.134) (subsection.143)]
+>> endobj
+923 0 obj <<
+/Names [(subsection.15) 70 0 R (subsection.16) 74 0 R (subsection.17) 78 0 R (subsection.18) 82 0 R (subsection.19) 86 0 R (subsection.20) 90 0 R]
+/Limits [(subsection.15) (subsection.20)]
+>> endobj
+924 0 obj <<
+/Names [(subsection.22) 98 0 R (subsection.23) 102 0 R (subsection.24) 106 0 R (subsection.26) 114 0 R (subsection.27) 118 0 R (subsection.34) 142 0 R]
+/Limits [(subsection.22) (subsection.34)]
+>> endobj
+925 0 obj <<
+/Names [(subsection.35) 146 0 R (subsection.36) 150 0 R (subsection.4) 38 0 R (subsection.40) 162 0 R (subsection.5) 42 0 R (subsection.54) 178 0 R]
+/Limits [(subsection.35) (subsection.54)]
+>> endobj
+926 0 obj <<
+/Names [(subsection.57) 186 0 R (subsection.6) 46 0 R (subsection.60) 194 0 R (subsection.61) 198 0 R (subsection.66) 206 0 R (subsection.73) 218 0 R]
+/Limits [(subsection.57) (subsection.73)]
+>> endobj
+927 0 obj <<
+/Names [(subsection.74) 222 0 R (subsection.80) 226 0 R (subsection.82) 234 0 R (subsection.84) 242 0 R (subsection.85) 246 0 R (subsection.86) 250 0 R]
+/Limits [(subsection.74) (subsection.86)]
+>> endobj
+928 0 obj <<
+/Names [(subsection.87) 254 0 R (subsection.9) 54 0 R (subsection.97) 274 0 R (subsection.98) 278 0 R (subsection.99) 282 0 R (subsubsection.10) 58 0 R]
+/Limits [(subsection.87) (subsubsection.10)]
+>> endobj
+929 0 obj <<
+/Names [(subsubsection.12) 62 0 R (subsubsection.14) 66 0 R (subsubsection.144) 354 0 R (subsubsection.147) 358 0 R (subsubsection.151) 362 0 R (subsubsection.28) 122 0 R]
+/Limits [(subsubsection.12) (subsubsection.28)]
+>> endobj
+930 0 obj <<
+/Names [(subsubsection.30) 126 0 R (subsubsection.31) 130 0 R (subsubsection.32) 134 0 R (subsubsection.33) 138 0 R (subsubsection.59) 190 0 R (subsubsection.64) 202 0 R]
+/Limits [(subsubsection.30) (subsubsection.64)]
+>> endobj
+931 0 obj <<
+/Names [(subsubsection.69) 210 0 R (subsubsection.8) 50 0 R (table.108) 660 0 R (table.109) 661 0 R (table.152) 755 0 R (table.155) 776 0 R]
+/Limits [(subsubsection.69) (table.155)]
+>> endobj
+932 0 obj <<
+/Names [(table.49) 499 0 R (table.62) 563 0 R (table.65) 564 0 R]
+/Limits [(table.49) (table.65)]
+>> endobj
+933 0 obj <<
+/Kids [893 0 R 894 0 R 895 0 R 896 0 R 897 0 R 898 0 R]
+/Limits [(Doc-Start) (Item.125)]
+>> endobj
+934 0 obj <<
+/Kids [899 0 R 900 0 R 901 0 R 902 0 R 903 0 R 904 0 R]
+/Limits [(Item.126) (cite.ctan:fix2col)]
+>> endobj
+935 0 obj <<
+/Kids [905 0 R 906 0 R 907 0 R 908 0 R 909 0 R 910 0 R]
+/Limits [(cite.ctan:flushend) (equation.Appendix.F.12c)]
+>> endobj
+936 0 obj <<
+/Kids [911 0 R 912 0 R 913 0 R 914 0 R 915 0 R 916 0 R]
+/Limits [(equation.Appendix.F.14a) (paragraph.13)]
+>> endobj
+937 0 obj <<
+/Kids [917 0 R 918 0 R 919 0 R 920 0 R 921 0 R 922 0 R]
+/Limits [(section*.101) (subsection.143)]
+>> endobj
+938 0 obj <<
+/Kids [923 0 R 924 0 R 925 0 R 926 0 R 927 0 R 928 0 R]
+/Limits [(subsection.15) (subsubsection.10)]
+>> endobj
+939 0 obj <<
+/Kids [929 0 R 930 0 R 931 0 R 932 0 R]
+/Limits [(subsubsection.12) (table.65)]
+>> endobj
+940 0 obj <<
+/Kids [933 0 R 934 0 R 935 0 R 936 0 R 937 0 R 938 0 R]
+/Limits [(Doc-Start) (subsubsection.10)]
+>> endobj
+941 0 obj <<
+/Kids [939 0 R]
+/Limits [(subsubsection.12) (table.65)]
+>> endobj
+942 0 obj <<
+/Kid
